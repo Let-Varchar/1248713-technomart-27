0d809ac66f059b5699ede16f2c20024cd82fc0fa
andy-max <andrey_deev@bk.ru>
2020-03-05 21:03:36 +0300

    Поправил замечания
    1. исправил меню. ВВысота формируется за счет padding
    2. полностью переверстал footer. Правильно ли сейчас? Написано что будут проверять по ширине 960px. при такой ширине всё идеально у краев
    3.  в фильтре ползунки сделал button
    4. переделал слайдеры, оживлять не буду.
    Проблема в слайдере что нижние точки, указывающие на положение слайды из button не становятся круглыми, при ширине и длине они 6px они вытянутые, помог padding 2px. Но есть другая проблема, как одновренно спозиционировать 2 элемента и расставить рядом? Они всегда залезают один под другой

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-let link = document.querySelector(".write_us");
-let modal = document.querySelector(".modal_writeus");
+let writeUs = document.querySelector(".write_us");
+let modal = document.querySelector(".modal_wrap");
 let close = modal.querySelector(".modal_close");
+let login = modal.querySelector("[name=popoup-name]");
+let mapLink = document.querySelector(".about_map");
+let mapModal = document.querySelector(".map_modal");
+let closeMap = mapModal.querySelector(".modal_close");
 
 
-link.addEventListener("click", function (evt){
+writeUs.addEventListener("click", function (evt){
   evt.preventDefault();
-  modal.classList.add("modal_writeus_show");
+  modal.classList.add("modal_wrap_show");
+  login.focus();
 });
 
 close.addEventListener("click", function (evt){
   evt.preventDefault();
-  modal.classList.remove("modal_writeus_show");
+  modal.classList.remove("modal_wrap_show");
+});
+
+mapLink.addEventListener("click", function(evt){
+  evt.preventDefault();
+  mapModal.classList.add("map_modal_show");
+});
+
+closeMap.addEventListener("click", function(evt){
+  evt.preventDefault();
+  mapModal.classList.remove("map_modal_show");
 });
