e349abf5f744967d59d57da6e424a8a0d86cb2b4
andy-max <andrey_deev@bk.ru>
2020-03-15 16:58:34 +0300

    всё поправил

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ let login = modal.querySelector("[name=popoup-name]");
 let mapLink = document.querySelector(".about_map");
 let mapModal = document.querySelector(".map_modal");
 let closeMap = mapModal.querySelector(".modal_close");
-
+let buyBtn = document.querySelector(".product_item_buy");
+let modalBuy = document.querySelector(".modal_buy");
 
 writeUs.addEventListener("click", function (evt){
   evt.preventDefault();
@@ -26,4 +27,9 @@ mapLink.addEventListener("click", function(evt){
 closeMap.addEventListener("click", function(evt){
   evt.preventDefault();
   mapModal.classList.remove("map_modal_show");
+});
+
+buyBtn.addEventListener("click", function(evt){
+  evt.preventDefault();
+  modalBuy.classList.add("modal_buy_show");
 });
